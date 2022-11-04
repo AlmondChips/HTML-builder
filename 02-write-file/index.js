08a5c558ae8074e77adf07302fb8d34948cd17b2
AlmondChips <chips6t@gmail.com>
2022-11-04 23:45:13 +0300

    feat: complete 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,30 @@
+const readline = require('node:readline');
+const fs = require('node:fs');
+const path = require('node:path');
+const { stdin: input, stdout: output } = require('node:process');
+const process = require('process');
+const { rmSync } = require('node:fs');
+
+const rl = readline.createInterface({ input, output });
+const writeStream = fs.createWriteStream(path.resolve(__dirname, 'newtxt.txt'));
+
+	rl.question("What would you like to write?\n", (answer) => {
+		writeStream.write(answer+'\n', ()=>{	
+				rl.setPrompt('Anything else? (press Ctrl+C or write "exit" to stop.)\n');
+				rl.prompt();
+				rl.on('line', (input) => {
+							if(input === 'exit'){
+								rl.close();
+							}else{
+								writeStream.write(input+'\n');
+								rl.setPrompt('Anything else? (press Ctrl+C or write "exit" to stop.\n');
+								rl.prompt();
+							}
+				})
+		});
+	});
+
+	rl.on('close', () =>{ console.log('Goodbye!')})
+
+
+
