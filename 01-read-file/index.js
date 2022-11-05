f2858b7fd9a63ae10bf0ad290b2658c858da4fb9
AlmondChips <chips6t@gmail.com>
2022-11-05 20:34:31 +0300

    refactor: change realisation by using data event on readStream

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,12 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const readable = require('node:stream');
+const os = require('node:os');
 
-const stream = fs.createReadStream(path.join(__dirname, './text.txt'));
+const stream = fs.createReadStream(path.join(__dirname, './text.txt'), 'utf-8');
 
-stream.on('readable', () => {
-  console.log(`${stream.read()}`);
-	stream.close();
+stream.on('data', (chunk)=>{
+console.log(chunk);
 });
 
+stream.on('end', ()=>{stream.close()});
