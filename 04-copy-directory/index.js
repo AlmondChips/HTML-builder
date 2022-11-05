8007cc7a19a076e74492bb1c6d95246f3c74b292
AlmondChips <chips6t@gmail.com>
2022-11-05 22:45:26 +0300

    feat: complete 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,15 @@
+const fs = require('node:fs/promises');
+const path = require('node:path');
 
+function copyDir(){
+	const copyPath = path.join(__dirname, 'files-copy');
+	fs.mkdir(copyPath, {recursive:true});
+	fs.readdir(path.join(__dirname,'files')).then((files)=>{
+		files.forEach((file)=>{
+			fs.copyFile(path.join(__dirname,'files',file),path.join(copyPath,file));
+		})
+	})
+
+}
+
+copyDir();
