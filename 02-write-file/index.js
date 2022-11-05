c22270892e3b049ac41f048e975bc84621dfe0f0
AlmondChips <chips6t@gmail.com>
2022-11-05 23:40:27 +0300

    refactor: remove test files

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,29 +2,26 @@ const readline = require('node:readline');
 const fs = require('node:fs');
 const path = require('node:path');
 const { stdin: input, stdout: output } = require('node:process');
-const process = require('process');
-const { rmSync } = require('node:fs');
+const process = require('node:process');
+
 
 const rl = readline.createInterface({ input, output });
-const writeStream = fs.createWriteStream(path.resolve(__dirname, 'newtxt.txt'));
+const writeStream = fs.createWriteStream(path.resolve(__dirname, 'text.txt'));
 
-	rl.question("What would you like to write?\n", (answer) => {
-		writeStream.write(answer+'\n', ()=>{	
-				rl.setPrompt('Anything else? (press Ctrl+C or write "exit" to stop.)\n');
-				rl.prompt();
-				rl.on('line', (input) => {
-							if(input === 'exit'){
-								rl.close();
-							}else{
-								writeStream.write(input+'\n');
-								rl.setPrompt('Anything else? (press Ctrl+C or write "exit" to stop.\n');
-								rl.prompt();
-							}
-				})
-		});
-	});
+  rl.question("What would you like to write?\n", (answer) => {
+    writeStream.write(answer+'\n', ()=>{	
+      rl.setPrompt('Anything else? (press Ctrl+C or write "exit" to stop.)\n');
+      rl.prompt();
+      rl.on('line', (input) => {
+          if(input === 'exit'){
+            rl.close();
+          }else{
+            writeStream.write(input+'\n');
+            rl.setPrompt('Anything else? (press Ctrl+C or write "exit" to stop.\n');
+            rl.prompt();
+          }
+      })
+    });
+  });
 
-	rl.on('close', () =>{ console.log('Goodbye!')})
-
-
-
+rl.on('close', () =>{ console.log('Goodbye!')})
