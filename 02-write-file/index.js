7e35040f3ef5c59d501f106f91a98790ed40301a
AlmondChips <chips6t@gmail.com>
2022-11-09 17:52:14 +0300

    fix: now if first console input is exit the program is end

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -9,6 +9,10 @@ const rl = readline.createInterface({ input, output });
 const writeStream = fs.createWriteStream(path.resolve(__dirname, 'text.txt'));
 
   rl.question("What would you like to write?\n", (answer) => {
+		if(answer === 'exit'){
+			rl.close();
+			return;
+		} 
     writeStream.write(answer+'\n', ()=>{	
       rl.setPrompt('Anything else? (press Ctrl+C or write "exit" to stop.)\n');
       rl.prompt();
